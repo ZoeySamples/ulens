c913c9ede4554d5842bb59a817127986fa0c7e29
samples.51 <samples.51@huggins.astronomy.osu.edu>
2018-06-04 18:45:41 -0400

    Switch to using class

--- BinaryLens/MakePlots.py
+++ BinaryLens/MakePlots.py
@@ -1,149 +1,149 @@
 # Zoey Samples
 # Created: May 30, 2018
 # BinaryLensMakePlots.py
-# Last Updated: May 31, 2018
+# Last Updated: Jun 04, 2018
 
 import numpy as np
 import cmath
 import matplotlib.pyplot as plt
-import MulensModel as mm
-import BinaryLensFunctions as blf
+import Functions as blf
 from astropy.io import fits
 
-def size_caustic(s, q, origin = 'geo_cent'):
-	w = 4.*np.sqrt(q)*(1. + 1./(2.*(s**2))) / (s**2)
-	h = 4.*np.sqrt(q)*(1. - 1./(2.*(s**2))) / (s**2)
-	x = 0.5*s - 1.0/s
-	return w, h, x
+class Plots(object):
+	
+	def __init__(self, s=None, q=None, origin='plan', pts=200, solver = 'numpy'):
+		self.s = s
+		self.q = q
+		self.pts = pts
+		self.origin = origin
+		self.solver = solver
+	
+	def get_variables(self):
+		(w_caustic, h_caustic, x_cent) = self.size_caustic()
+		self.x_grid = np.linspace(x_cent - w_caustic, 
+								x_cent + w_caustic, self.pts)
+		self.y_grid = np.linspace(-h_caustic, h_caustic, self.pts)
+		self.x_1d = np.zeros(self.pts**2)
+		self.y_1d = np.zeros(self.pts**2)
+		solutions = []
+		for (i, xx) in enumerate(self.x_grid):
+			for (j, yy) in enumerate(self.y_grid):
+				idx = self.pts*i + j
+				solutions.append(blf.solution(x=xx, y=yy, s=self.s, q=self.q,
+										origin=self.origin, solver=self.solver))
+				self.x_1d[idx] = xx
+				self.y_1d[idx] = yy
+		self.solutions = solutions
 
-	"""
-	if origin == 'geo_cent':
-		x = 0.5*s - 1.0/s
-	elif origin == 'star':
-		x = s - 1.0/s
-	elif origin == 'plan':
-		x = -1.0/s
-	elif origin == 'com':
-		x = 2.0*s/(1.0 + q) - 1.0/s
-	else:
-		print('Not a valid coordinate system. Converting to geometric center frame')
-		x = 0.5*s - 1.0/s
-	"""
+	def size_caustic(self):
+		w = 4.*np.sqrt(self.q)*(1. + 1./(2.*(self.s**2))) / (self.s**2)
+		h = 4.*np.sqrt(self.q)*(1. - 1./(2.*(self.s**2))) / (self.s**2)
+		x = 0.5*self.s - 1.0/self.s
+		return w, h, x
 
-def plot_n_solns(s, q, origin = 'geo_cent', solver='numpy', pts=150):
-	"""
-	Plot showing number of images on a grid (x,y) around planetary caustic
+	def construct_plots(self):
+		self.get_variables()
+		self.num_images = np.zeros(self.pts**2, dtype=int)
+		self.mag_1d = np.zeros(self.pts**2, dtype=float)
+		for idx in range(self.pts**2):
+			(dm, m, zeta, z1, z2) = blf.assign(x=self.x_1d[idx], y=self.y_1d[idx], 
+									s=self.s, q=self.q, origin=self.origin)
+			self.mag_1d[idx] = blf.magnification(x=self.x_1d[idx], y=self.y_1d[idx],
+									s=self.s, q=self.q, origin=self.origin)
+			for z in self.solutions[idx]:
+				if blf.check_solution(dm, m, zeta, z1, z2, z, self.origin):
+					self.num_images[idx] += 1
 
-	"""
-	(w_caustic, h_caustic, x_cent) = size_caustic(s, q, origin)
-	x_grid = np.linspace(x_cent - w_caustic, x_cent + w_caustic, pts)
-	y_grid = np.linspace(-h_caustic, h_caustic, pts)
-	x_1d = np.zeros(pts**2)
-	y_1d = np.zeros(pts**2)
-	im_num = np.zeros(pts**2, dtype=int)
-	color = np.zeros(pts**2)
-	num_one = 0
-	num_two = 0
-	num_four = 0
-	num_tot = 0
-	print('parameters:\ns={:}\nq={:}\n'.format(s, q))
-	for (i, xx) in enumerate(x_grid):
-		for j, yy in enumerate(y_grid):
-			idx = pts*i + j
-			x_1d[idx] = xx
-			y_1d[idx] = yy
-			(dm, m, zeta, z1, z2) = blf.assign(xx, yy, s, q, origin)
-			solutions = blf.solution(xx, yy, s, q, origin, solver)
-			for z in solutions:
-				if blf.check_solution(dm, m, zeta, z1, z2, z, origin):
-					im_num[idx] += 1
-			if im_num[idx] == 5:
-				color[idx] = 255
-			elif im_num[idx] == 3:
-				color[idx] = 1
-			elif im_num[idx] == 4:
-				color[idx] = 120
+	def print_errors(self):
+		num_one = num_two = num_four = 0
+		for i in self.num_images:
+			if (i==3) or (i==5):
+				continue
+			elif i == 4:
 				num_four += 1
-			elif im_num[idx] == 2:
-				color[idx] = 120
+			elif i == 2:
 				num_two += 1
-			elif im_num[idx] == 1:
+			elif i == 1:
 				num_one += 1
 			else:
-				print('Concern: found {:} solutions'.format(im_num[idx]))
-	num_tot = num_one + num_two + num_four
-	print('Concern: number of points where the number of images is',
-		'\n1: {:}\n2: {:}\n4: {:}\ntotal: {:} out of {:} points'
-		.format(num_one, num_two, num_four, num_tot, pts**2))
-	plt.scatter(x_1d, y_1d, c=im_num, s=((500./pts)**2), marker = 'o', cmap='jet', lw=None)
-	im_plot = plt.colorbar()
-	im_plot.set_label('Num Images')
-	plt.xlabel('X-position of source')
-	plt.ylabel('Y-position of source')
-	plt.xlim(x_cent - w_caustic, x_cent + w_caustic)
-	plt.ylim(-h_caustic, h_caustic)
-	plt.title('Num Images using "{}" frame'.format(origin))
+				print('Concern: found {:} solutions'.format(i))
+		num_tot = num_one + num_two + num_four
+		print('Concern: number of points where the number of images is',
+				'\n1: {:}\n2: {:}\n4: {:}\ntotal: {:} out of {:} points'
+				.format(num_one, num_two, num_four, num_tot, (self.pts)**2))
 
-def plot_magnification(s, q, origin = 'geo_cent', solver='numpy', pts=150):
-	"""Make square grid of points that shows the magnification at each point"""
-	(w_caustic, h_caustic, x_cent) = size_caustic(s, q, origin)
-	x_grid = np.linspace(x_cent - w_caustic, x_cent + w_caustic, pts)
-	y_grid = np.linspace(-h_caustic, h_caustic, pts)
-	x_1d = np.zeros(pts**2)
-	y_1d = np.zeros(pts**2)
-	mag_1d = np.zeros(pts**2)
-	for i, xx in enumerate(x_grid):
-		for j, yy in enumerate(y_grid):
-			idx = pts*i + j
-			mag_1d[idx] = blf.magnification(x=xx, y=yy, s=s, q=q, origin=origin, solver=solver)
-			x_1d[idx] = xx
-			y_1d[idx] = yy
-	plt.scatter(x_1d, y_1d, c=mag_1d, s=((500./pts)**2), marker = 'o', cmap='jet', lw=None)
-	mag_plot = plt.colorbar()
-	mag_plot.set_label('Magnification')
-	plt.xlabel('X-position of source')
-	plt.ylabel('Y-position of source')
-	plt.xlim(x_cent - w_caustic, x_cent + w_caustic)
-	plt.ylim(-h_caustic, h_caustic)
-	plt.title('Magnification using "{}" frame'.format(origin))
+	def plot_n_solns(self):
+		"""
+		Plot showing number of images on a grid (x,y) around planetary caustic
 
-def write_to_fits(s, q, origin = 'geo_cent', solver='numpy', pts=500):
-	"""
-	Writes information about grid to a .fits table for comparison of magnification
-	and number of images between different coordinate systems and solving methods.
-	"""
-	(w_caustic, h_caustic, x_cent) = size_caustic(s, q, origin)
-	x_grid = np.linspace(x_cent - w_caustic, x_cent + w_caustic, pts)
-	y_grid = np.linspace(-h_caustic, h_caustic, pts)
-	x_1d = np.zeros(pts**2)
-	y_1d = np.zeros(pts**2)
-	mag_1d = np.zeros(pts**2)
-	im_num = np.zeros(pts**2, dtype=int)
-	print('parameters:\ns={:}\nq={:}\n'.format(s, q))
-	for (i, xx) in enumerate(x_grid):
-		for j, yy in enumerate(y_grid):
-			idx = pts*i + j
-			mag_1d[idx] = blf.magnification(x=xx, y=yy, s=s, q=q, origin=origin,
-											solver=solver)
-			x_1d[idx] = xx
-			y_1d[idx] = yy
-			(dm, m, zeta, z1, z2) = blf.assign(xx, yy, s, q, origin)
-			solutions = blf.solution(xx, yy, s, q, origin, solver)
-			for z in solutions:
-				if blf.check_solution(dm, m, zeta, z1, z2, z, origin):
-					im_num[idx] += 1
-	col = []
-	col.append(fits.Column(name='x', array=x_1d, format='D'))
-	col.append(fits.Column(name='y', array=y_1d, format='D'))
-	col.append(fits.Column(name='Magnification', array=mag_1d, format='D'))
-	col.append(fits.Column(name='Number Images', array=im_num, format='I'))
-	hdu1 = fits.BinTableHDU.from_columns(col)
-	hdr = fits.Header()
-	hdr['SEPARAT'] = '{:f}'.format(s)
-	hdr['M_RATIO'] = '{:f}'.format(q)
-	hdr['ORIGIN'] = origin
-	hdr['SOLVER'] = solver
-	hdu0 = fits.PrimaryHDU(header = hdr)
-	hdus = fits.HDUList([hdu0, hdu1])
-	hdus.writeto('BinaryLens2.fits')
+		"""
 
+		self.get_variables()
+		self.construct_plots()
+		self.print_errors()
+		plt.scatter(self.x_1d, self.y_1d, c=self.num_images, s=((500./self.pts)**2), 
+						marker = 'o', cmap='jet', lw=None)
+		im_plot = plt.colorbar()
+		im_plot.set_label('Num Images')
+		plt.xlabel('X-position of source')
+		plt.ylabel('Y-position of source')
+		(w_caustic, h_caustic, x_cent) = self.size_caustic()
+		plt.xlim(x_cent - w_caustic, x_cent + w_caustic)
+		plt.ylim(-h_caustic, h_caustic)
+		plt.title('Num Images using "{}" frame'.format(self.origin))
+
+	def plot_magnification(self):
+		"""
+		Make square grid of points that shows the magnification at each point
+
+		"""
+
+		self.get_variables()
+		self.construct_plots()
+		plt.scatter(self.x_1d, self.y_1d, c=self.mag_1d, s=((500./self.pts)**2),
+								marker = 'o', cmap='jet', lw=None)
+		mag_plot = plt.colorbar()
+		mag_plot.set_label('Magnification')
+		plt.xlabel('X-position of source')
+		plt.ylabel('Y-position of source')
+		(w_caustic, h_caustic, x_cent) = self.size_caustic()
+		plt.xlim(x_cent - w_caustic, x_cent + w_caustic)
+		plt.ylim(-h_caustic, h_caustic)
+		plt.title('Magnification using "{}" frame'.format(self.origin))
+
+	def write_to_fits(self):
+		"""
+		Writes information about grid to a .fits table for comparison of magnification
+		and number of images between different coordinate systems and solving methods.
+		"""
+
+		self.get_variables()
+		self.construct_plots()
+		col = []
+		col.append(fits.Column(name='x', array=self.x_1d, format='D'))
+		col.append(fits.Column(name='y', array=self.y_1d, format='D'))
+		col.append(fits.Column(name='Magnification', array=self.mag_1d, format='D'))
+		col.append(fits.Column(name='Number Images', array=self.num_images, format='I'))
+		hdu1 = fits.BinTableHDU.from_columns(col)
+		hdr = fits.Header()
+		hdr['SEPARAT'] = '{:f}'.format(self.s)
+		hdr['M_RATIO'] = '{:f}'.format(self.q)
+		hdr['ORIGIN'] = self.origin
+		hdr['SOLVER'] = self.solver
+		hdr['RES'] = '{:f}'.format(self.pts)
+		hdu0 = fits.PrimaryHDU(header = hdr)
+		hdus = fits.HDUList([hdu0, hdu1])
+
+		# Abbreviate file name
+		if self.origin == 'geo_cent':
+			origin_str = 'gcent'
+		else:
+			origin_str = self.origin
+		if self.solver == 'numpy':
+			solver_str = 'np'
+		elif self.solver == 'Skowron_and_Gould_12':
+			solver_str = 'SG'
+		else:
+			 solver_str = solver
+		hdus.writeto('../Tables/BL_{}_{:2}.fits'.format(origin_str, solver_str))
+		
--- BinaryLens/Plots.py
+++ BinaryLens/Plots.py
@@ -1,56 +1,73 @@
 # Zoey Samples
 # Created: May 22, 2018
 # BinaryLensPlots.py
-# Last Updated: May 31, 2018
+# Last Updated: Jun 04, 2018
 
 import numpy as np
 import cmath
 import matplotlib.pyplot as plt
-import MulensModel as mm
-import BinaryLensFunctions as blf
-import BinaryLensMakePlots as blp
+import Functions as blf
+from MakePlots import Plots
 
 # Input parameters
 s = 1.9		# Separation between bodies. Assume s>1. Type = string
 q = 1.e-7	# mass ratio between bodies in units of larger body's mass. Type = float
-pts = 20	# Number of data points on each side of the grid. Type = int
+pts = 300	# Number of data points on each side of the grid. Type = int
 origin = 'plan'		#Coordinate frame to carry out calculations. Type = string
+solver = 'Skowron_and_Gould_12'	# Determines which method to solve polynomial with.
 
 """
-Options:
+origin options:
 
 	'geo_cent' - the geometric center frame [default if not specified]
 	'star' - the star's (or larger body's) frame
 	'plan' - the planet's (or smaller body's) frame
 	'com' - the center-of-mass frame
 
-"""
-#method = ['numpy', 'Skowron_and_Gould_12']
-solver = 'Skowron_and_Gould_12'
-#solver = 'numpy'
-#solver = 'zroots'		# Not precise enough to be useful on this computer
 
-"""
-Determines which method to solve polynomial with.
-Options:
+solver options:
 
 	'numpy'					- Uses np.roots method
 	'Skowron_and_Gould_12'	- Uses Skowron & Gould 2012 method
-	'zroots'				- Uses zroots laguerre method
+	'zroots'				- Uses zroots laguerre method (not precise enough
+								to be useful on this computer
 
 """
+
+params = []
+plot = []
+
+loop_on = True
+if loop_on:
+	method =  ['numpy', 'Skowron_and_Gould_12']
+	coordinates = ['geo_cent', 'plan']
+	for (i, solver) in enumerate(method):
+		for (j, origin) in enumerate(coordinates):
+			params.append({'s': s, 'q': q, 'pts': pts, 'origin': origin,
+							'solver': solver})
+			plot.append(Plots(**params[-1]))
+else:
+	params.append({'s': s, 'q': q, 'pts': pts, 'origin': origin, 'solver': solver})
+	plot.append(Plots(**params[-1]))
 
 # Plots the number of solutions in a grid of points centered on the caustic
 plot_on = False
 if plot_on:
-	for solver in method:
-		blp.plot_n_solns(s, q, origin, solver, pts)
+	for (i, p) in enumerate(plot):
+		print((params[i]))
+		p.plot_n_solns()
 		plt.show()
 
 # Plots magnification in a grid of points centered on the caustic
 plot_on = False
 if plot_on:
-	blp.plot_magnification(s=s, q=q, origin=origin, solver=solver, pts=pts)
-	plt.show()
+	for (i, p) in enumerate(plot):
+		print((params[i]))
+		p.plot_magnification()
+		plt.show()
 
-blp.write_to_fits(s, q, origin = 'geo_cent', solver='numpy', pts=500)
+# Writes data to fits table
+plot_on = True
+if plot_on:
+	for p in plot:
+		p.write_to_fits()
