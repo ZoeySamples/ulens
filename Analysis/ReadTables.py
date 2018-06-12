b6404698de3aeae0b34cf60e88597b5985eceb9d
samples.51 <samples.51@huggins.astronomy.osu.edu>
2018-06-12 18:34:05 -0400

    New save plot feature

--- Analysis/ReadTables.py
+++ Analysis/ReadTables.py
@@ -4,6 +4,7 @@
 # Last Updated: Jun 11, 2018
 
 import sys
+import os
 import matplotlib.pyplot as plt
 from BinaryLens import BinaryLens as BL
 from HDUL import HDUL
@@ -15,6 +16,24 @@ for i in range(len(file_name)):
 	param = {'file_name': file_name[i], 'cutoff':1000}
 	hdul.append(HDUL(**param))
 
-hdul[0].plot_n_solutions_errors()
-hdul[0].plot_magn_outliers()
-plt.show()
+for i in range(len(hdul)):
+	hdul[i].plot_n_solutions_errors()
+	hdul[i].plot_magn_outliers()
+	plt.gcf().set_size_inches(10, 6)
+	j = 0
+	while True:
+		j += 1
+		file_name = ('../Tables/errors_{}_{}{}.png'.format(hdul[i].solver_file,
+								hdul[i].origin_file, j))
+		path = os.path.abspath(__file__)
+		for k in range(2):
+			path = os.path.dirname(path)
+		file_path = os.path.join(path, file_name[3:])
+		if os.path.exists(file_path):
+		    continue
+		plt.savefig(file_name)
+		plt.show()
+		print(file_name, 'has been saved')
+		break
+
+
