a7b3c255496ac4ebc4c079572e8a3b1a463bf80f
samples.51 <samples.51@huggins.astronomy.osu.edu>
2018-07-09 15:58:20 -0400

    Small changes

--- Analysis/caustic_test.py
+++ Analysis/caustic_test.py
@@ -1,7 +1,7 @@
 # Zoey Samples
 # Created: Jun 7, 2018
 # caustic_test.py
-# Last Updated: Jun 25, 2018
+# Last Updated: Jul 09, 2018
 
 from BinaryLens import BinaryLens as BL
 from TripleLens import TripleLens as TL
@@ -10,43 +10,37 @@ import MulensModel as mm
 import matplotlib.pyplot as plt
 
 solver = 'SG12'
-originBL = 'star'
+originBL = 'plan'
 originTL = 'body3'
 
-plot_frame='caustic'
+plot_frame = 'caustic'
+region = 'caustic2a'
 
-s=0.8
-q=1e-5
+s=1.4
+q=1e-14
 
-s1=0.8
-s2=0.8
-q1=1e-5
-q2=1e-1
-phi=135
+s1=s
+s2=1.2
+q1=q
+q2=1e-3
+phi=0
 system='SPM'
 
-param = ({'s': s, 'q': q, 'origin': originBL, 'solver': solver,
+param = ({'s': s, 'q': q, 'origin': originBL, 'region': region, 'solver': solver,
 		  'plot_frame': plot_frame})
 testBL = BL(**param)
 causticBL = Caus(lens=testBL)
 
 param = ({'s1': s1, 's2': s2, 'q1': q1,  'q2': q2, 'phi': phi,
-		'origin': originTL, 'solver': solver, 'system': system,
+		'origin': originTL, 'region': region, 'solver': solver, 'system': system,
 		'plot_frame': plot_frame})
 testTL = TL(**param)
-causticTL = Caus(lens=testTL)
+causticTL = Caus(lens=testTL, solver=solver)
 
 # Plot the caustic alone
 causticBL.plot_caustic(s=1, color='blue')
-causticTL.plot_caustic(s=1, color='red')
+#causticTL.plot_caustic(s=1, color='red')
 plt.show()
-
-# Plot the position of each body via the Caustics class
-plt.scatter(causticTL.z1.real, causticTL.z1.imag, color='red', s=50)
-plt.scatter(causticTL.z2.real, causticTL.z2.imag, color='orange', s=10)
-plt.scatter(causticTL.z3.real, causticTL.z3.imag, color='blue', s=3)
-#plt.show()
-
 
 #Interesting parameters:
 solver = 'SG12'
