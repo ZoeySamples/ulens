d45302d0d03e29627e3e6eb116606802acba81cc
samples.51 <samples.51@huggins.astronomy.osu.edu>
2018-07-09 15:58:51 -0400

    New features for caustics

--- Solvers/VBBL/VBBinaryLensingLibrary_wrapper.cpp
+++ Solvers/VBBL/VBBinaryLensingLibrary_wrapper.cpp
@@ -67,3 +67,54 @@ extern "C" double* VBBL_SG12_10(double p0, double p1, double p2, double p3,
     return roots;
 
 }
+
+extern "C" double* VBBL_SG12_4(double p0, double p1, 
+                    double p2, double p3, double p4, double p5, double p6, 
+                    double p7, double p8, double p9) {
+    static VBBinaryLensing VBBL;
+    complex complex_poly[5], complex_roots[4];
+    static double roots[8];
+    int i;
+
+    complex_poly[0] = complex(p0, p5);
+    complex_poly[1] = complex(p1, p6);
+    complex_poly[2] = complex(p2, p7);
+    complex_poly[3] = complex(p3, p8);
+    complex_poly[4] = complex(p4, p9);
+
+    VBBL.cmplx_roots_gen(complex_roots, complex_poly, 4, true, true);
+    
+    for (i=0; i<4; i++) {
+        roots[i] = complex_roots[i].re;
+        roots[i+4] = complex_roots[i].im;
+    }
+
+    return roots;
+}
+
+extern "C" double* VBBL_SG12_6(double p0, double p1, 
+                    double p2, double p3, double p4, double p5, double p6, 
+                    double p7, double p8, double p9, double p10, double p11,
+					double p12, double p13) {
+    static VBBinaryLensing VBBL;
+    complex complex_poly[7], complex_roots[6];
+    static double roots[12];
+    int i;
+
+    complex_poly[0] = complex(p0, p7);
+    complex_poly[1] = complex(p1, p8);
+    complex_poly[2] = complex(p2, p9);
+    complex_poly[3] = complex(p3, p10);
+    complex_poly[4] = complex(p4, p11);
+    complex_poly[5] = complex(p5, p12);
+    complex_poly[6] = complex(p6, p13);
+
+    VBBL.cmplx_roots_gen(complex_roots, complex_poly, 6, true, true);
+    
+    for (i=0; i<6; i++) {
+        roots[i] = complex_roots[i].re;
+        roots[i+6] = complex_roots[i].im;
+    }
+
+    return roots;
+}
