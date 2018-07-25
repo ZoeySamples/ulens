9507ca6f5c5c948d8f5c9ad2f47ebe83ef5ba647
samples.51 <samples.51@huggins.astronomy.osu.edu>
2018-07-24 20:32:22 -0400

    New files

---
+++ Findings/TL/MassSeparation.py
@@ -0,0 +1,199 @@
+# Zoey Samples
+# Created: Jun 21, 2018
+# OriginInfo.py
+# Last Updated: Jul 2, 2018
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import matplotlib.colors as colors
+import numpy as np
+from TripleLens import TripleLens as TL
+from Caustics import Caustics as caus
+from pathlib import Path
+
+"""
+This file shows plots for the planet frame, using the SG12 solver,for 
+varying separations and mass ratios to show how well the modeling works.
+"""
+
+def num_images_demo():
+
+	param = [[None] * len(separations) for j in range(len(mass_ratios))]
+	plot = [[None] * len(separations) for j in range(len(mass_ratios))]
+	fig, ax = plt.subplots(len(mass_ratios), len(separations))
+
+	for (i, q) in enumerate(mass_ratios):
+		for (j, s) in enumerate(separations):
+			idx = 1 + j + len(separations)*i
+
+			# Initialize each binary lens system with the BinaryLens class.
+			if idx == 2:
+				region = regions[1]
+			else:
+				region = regions[0]
+
+			param[i][j] = ({'s2': s2, 's1': s1, 'phi': phi, 'q2': q2,
+					'q1': q1, 'res': res, 'origin': origin,
+					'region': region, 'region_lim': region_lim,
+					'solver': solver, 'SFD': SFD, 'system': system,
+					'plot_frame': plot_frame, 'refine_region': refine_region})
+			plot[i][j] = TL(**param[i][j])
+
+			cmap = plt.cm.Blues
+			cmaplist = [cmap(i) for i in range(cmap.N)]
+			cmap = cmap.from_list('Custom cmap', cmaplist, cmap.N)
+			bounds = np.linspace(-0.5,5.5,7)
+			norm = colors.BoundaryNorm(bounds, cmap.N)
+			ticks = np.linspace(0,5,6)
+
+			# Get the data for the plots.
+			kwargs = plot[i][j].check_kwargs()
+			kwargs['cmap'] = cmap
+			kwargs['norm'] = norm
+			kwargs['s'] = 1
+			kwargs['lw'] = 0
+			plot[i][j].get_position_arrays()
+			plot[i][j].get_num_images_array()
+			(x, y, num_images) = (plot[i][j].x_array, plot[i][j].y_array,
+						plot[i][j].num_images)
+
+			# Create and adjust the plots appropriately.
+			ax[i][j] = plt.subplot(len(mass_ratios), len(separations), idx)
+			sc = ax[i][j].scatter(x, y, c=num_images, vmin=0, vmax=5, **kwargs)
+		#	caustic = caus(lens=plot[i][j], solver='SG12')
+		#	caustic.plot_caustic(s=1, color='yellow', points=2000, lw=0)
+			get_plot_parameters(plot=plot[i][j], ax=ax[i][j], i=i, j=j)
+
+	# Add an axis for the color bar.
+	cbar = fig.add_axes([0.12, 0.90, 0.60, 0.035])
+	num_color = plt.colorbar(sc, cax=cbar, cmap=kwargs['cmap'], ticks=ticks,
+				orientation='horizontal')
+	num_color.set_label('Number of Images', fontsize=16, labelpad=-62)
+	cbar.axes.tick_params(labelsize=12)
+	get_plot_text(plot, fig)
+
+	# Save the plot as a .png file.
+	if save_fig:
+		file_name = '../../Tables/TL_images_plan_.png'
+		save_png(file_name)
+
+	if show_fig:
+		plt.show()
+
+def magnification_demo():
+
+	param = [[None] * len(separations) for j in range(len(mass_ratios))]
+	plot = [[None] * len(separations) for j in range(len(mass_ratios))]
+	fig, ax = plt.subplots(len(mass_ratios), len(separations))
+
+	for (i, q) in enumerate(mass_ratios):
+		for (j, s) in enumerate(separations):
+			idx = 1 + j + len(separations)*i
+
+			# Initialize each binary lens system with the BinaryLens class.
+			if idx == 2:
+				region = regions[1]
+			else:
+				region = regions[0]
+
+			param[i][j] = ({'s': s, 'q': q, 'res': res, 'origin': origin,
+					'region': region, 'region_lim': region_lim, 
+					'solver': solver, 'SFD': SFD, 'refine_region': refine_region})
+			plot[i][j] = TL(**param[i][j])
+
+			# Get the data for the plots.
+			cmap = plt.cm.YlOrRd
+			cmaplist = [cmap(i) for i in range(cmap.N)]
+			cmap = cmap.from_list('Custom cmap', cmaplist, cmap.N)
+			ticks = np.array([1,10,100])
+
+			kwargs = plot[i][j].check_kwargs()
+			kwargs['cmap'] = cmap
+			kwargs['norm'] = colors.LogNorm()
+			kwargs['s'] = 1
+			kwargs['lw'] = 0
+			plot[i][j].get_position_arrays()
+			plot[i][j].get_magnification_array()
+			(x, y, magnification) = (plot[i][j].x_array, plot[i][j].y_array,
+						plot[i][j].magn_array)
+
+			# Create and adjust the plots appropriately.
+			ax[i][j] = plt.subplot(len(mass_ratios), len(separations), idx)
+			sc = ax[i][j].scatter(x, y, c=magnification, vmin=1, vmax=200, **kwargs)
+			get_plot_parameters(plot=plot[i][j], ax=ax[i][j], i=i, j=j)
+
+	# Add an axis for the color bar.
+	cbar = fig.add_axes([0.12, 0.90, 0.60, 0.035])
+	num_color = plt.colorbar(sc, cax=cbar, cmap=kwargs['cmap'], ticks=ticks,
+				orientation='horizontal')
+	num_color.set_label('Magnification', fontsize=15, labelpad=-62)
+	cbar.axes.tick_params(labelsize=12)
+	get_plot_text(plot, fig)
+
+	# Save the plot as a .png file.
+	if save_fig:
+		file_name = '../../Tables/TL_magn_plan_.png'
+		save_png(file_name)
+
+	if show_fig:
+		plt.show()
+
+def get_plot_text(plot, fig):
+
+	for (i, q) in enumerate(mass_ratios):
+		fig.text(0.925, 0.81 - .33/len(mass_ratios) - .77*i/len(mass_ratios),
+				'q={:.0e}'.format(q), ha='center', va='center', fontsize=16)
+	fig.text(0.79, 0.905, '{} Frame\n{} Solver'.format(plot[0][0].origin_title,
+				plot[0][0].solver_title), fontsize=16)
+	plt.subplots_adjust(wspace=0.35, hspace=0.23, top=0.82, bottom=0.06,
+				left=0.12, right=0.88)
+	plt.gcf().set_size_inches(2.8*len(separations)+0.5, 1.8*len(mass_ratios)+1.0)
+
+def get_plot_parameters(plot, ax, i, j):
+
+	(xmin, xmax) = (min(plot.x_array), max(plot.x_array))
+	(ymin, ymax) = (min(plot.y_array), max(plot.y_array))
+	(dx, dy) = (xmax-xmin, ymax-ymin)
+	plt.xlim(xmin, xmax)
+	plt.ylim(ymin, ymax)
+	plt.xticks(np.arange(-0.3*dx, xmax, 0.6*dx))
+	plt.yticks(np.arange(-0.25*dy, ymax, 0.5*dy))
+	ax.tick_params(axis='x', labelsize=11)
+	ax.tick_params(axis='y', labelsize=11)
+	ax.axes.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.1e'))
+	ax.axes.xaxis.set_major_formatter(mtick.FormatStrFormatter('%.1e'))
+	labels = ax.get_yticklabels()
+	plt.setp(labels, rotation=60, verticalalignment='center', stretch='condensed',  x=0.05)
+	labels = ax.get_xticklabels()
+	plt.setp(labels, stretch='condensed')
+	if (i == 0):
+		ax.axes.set_title('s={}'.format(separations[j]), fontsize=16)
+
+def save_png(file_name):
+
+	for i in range(10):
+		name = file_name[:-4] + '{}'.format(i) + file_name[-4:]
+		if Path(name).is_file():
+			continue
+		plt.savefig(name, dpi=300)
+		print(name, 'has been saved')
+		return
+	print('Error: too many files of same name already exist. File not saved')
+
+# Here are the input parameters for making the plots.
+separations = [0.6, 0.9, 1.1, 2.0, 5.0]
+mass_ratios = [1e-1, 1e-3, 1e-7, 1e-12]
+origin = 'plan'
+res = int(10)
+solver =  'SG12'
+regions = ['caustic_a', 'custom_a']
+region_lim = [-2, 2.8, -5.08, 2.2]
+save_fig = False
+show_fig = True
+
+refine_region = True
+SFD = True
+num_images_demo()
+magnification_demo()
+
+
---
+++ Findings/TL/SolverOrigin.py
@@ -0,0 +1,248 @@
+# Zoey Samples
+# Created: Jun 21, 2018
+# OriginInfo.py
+# Last Updated: Jul 17, 2018
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import matplotlib.colors as colors
+import matplotlib.gridspec as gridspec
+from itertools import product
+import numpy as np
+from TripleLens import TripleLens as TL
+from Caustics import Caustics as caus
+from pathlib import Path
+
+
+def num_images_demo():
+
+	num_outer_plots = len(ps_mass_ratios)*len(mp_mass_ratios)
+	num_inner_plots = len(origins)*len(solvers)
+	fig = plt.figure(figsize=(10, 8))
+	outer = gridspec.GridSpec(len(ps_mass_ratios), len(mp_mass_ratios),
+							  wspace=0.25, hspace=0.38)
+	param = [[None]*num_inner_plots for j in range(num_outer_plots)]
+	plot = [[None]*num_inner_plots for j in range(num_outer_plots)]
+
+	for (i, q1), (j, q2) in product(enumerate(ps_mass_ratios), enumerate(mp_mass_ratios)):
+		outer_idx = j + i*len(mp_mass_ratios)
+		inner = gridspec.GridSpecFromSubplotSpec(len(origins), len(solvers),
+					subplot_spec=outer[outer_idx], wspace=0.1, hspace=0.25)
+
+		for (k, origin), (l, solver) in product(enumerate(origins), enumerate(solvers)):
+			inner_idx = l + k*len(solvers)
+			(m, n) = (outer_idx, inner_idx)
+
+			# Initialize each triple lens system with the TripleLens class.
+			param[m][n] = ({'s2': s2, 's1': s1, 'phi': phi, 'q2': q2,
+					'q1': q1, 'res': res, 'origin': origin,
+					'region': region, 'region_lim': region_lim,
+					'solver': solver, 'SFD': SFD, 'system': system,
+					'plot_frame': plot_frame, 'refine_region': refine_region})
+			plot[m][n] = TL(**param[m][n])
+
+			cmap = plt.cm.Blues
+			cmaplist = [cmap(i) for i in range(cmap.N)]
+			cmap = cmap.from_list('Custom cmap', cmaplist, cmap.N)
+			bounds = np.linspace(-0.5,10.5,12)
+			norm = colors.BoundaryNorm(bounds, cmap.N)
+			ticks = np.linspace(0,10,11)
+
+			kwargs = plot[m][n].check_kwargs()
+			kwargs['cmap'] = cmap
+			kwargs['norm'] = norm
+			kwargs['s'] = 1
+			kwargs['lw'] = 0
+
+			# Get the data for the plots.
+			plot[m][n].get_position_arrays()
+			plot[m][n].get_num_images_array()
+			(x, y, num_images) = (plot[m][n].x_array, plot[m][n].y_array,
+								  plot[m][n].num_images)
+
+			# Create and adjust the plots appropriately.
+			ax = plt.subplot(inner[n])
+			sc = ax.scatter(x, y, c=num_images, vmin=0, vmax=10, **kwargs)
+	#		caustic = caus(lens=plot[m][n], solver='SG12')
+	#		caustic.plot_caustic(s=1, color='yellow', points=5000, lw=0)
+			get_inner_plot_parameters(plot=plot[m][n], ax=ax, k=k, l=l)
+			fig.add_subplot(ax)
+
+	# Add an axis for the color bar.
+	cbar = fig.add_axes([0.08, 0.895, 0.60, 0.04])
+	num_color = plt.colorbar(sc, cax=cbar, cmap=kwargs['cmap'], ticks=ticks,
+							 orientation='horizontal')
+	num_color.set_label('Number of Images', fontsize=15, labelpad=-66)
+	cbar.axes.tick_params(labelsize=12)
+	get_plot_text(plot, fig)
+
+	# Save the plot as a .png file.
+	if save_fig:
+		file_name = '../../Tables/TL_images_solver_.png'
+		save_png(file_name)
+
+	if show_fig:
+		plt.show()
+
+def magnification_demo():
+
+	num_outer_plots = len(ps_mass_ratios)*len(mp_mass_ratios)
+	num_inner_plots = len(origins)*len(solvers)
+	fig = plt.figure(figsize=(10, 8))
+	outer = gridspec.GridSpec(len(ps_mass_ratios), len(mp_mass_ratios),
+							  wspace=0.25, hspace=0.38)
+	param = [[None]*num_inner_plots for j in range(num_outer_plots)]
+	plot = [[None]*num_inner_plots for j in range(num_outer_plots)]
+
+	for (i, q1), (j, q2) in product(enumerate(ps_mass_ratios), enumerate(mp_mass_ratios)):
+		outer_idx = j + i*len(mp_mass_ratios)
+		inner = gridspec.GridSpecFromSubplotSpec(len(origins), len(solvers),
+					subplot_spec=outer[outer_idx], wspace=0.1, hspace=0.25)
+
+		for (k, origin), (l, solver) in product(enumerate(origins), enumerate(solvers)):
+			inner_idx = l + k*len(solvers)
+			(m, n) = (outer_idx, inner_idx)
+
+			# Initialize each triple lens system with the TripleLens class.
+			param[m][n] = ({'s2': s2, 's1': s1, 'phi': phi, 'q2': q2,
+					'q1': q1, 'res': res, 'origin': origin,
+					'region': region, 'region_lim': region_lim,
+					'solver': solver, 'SFD': SFD, 'system': system,
+					'plot_frame': plot_frame, 'refine_region': refine_region})
+			plot[m][n] = TL(**param[m][n])
+
+			cmap = plt.cm.YlOrRd
+			cmaplist = [cmap(i) for i in range(cmap.N)]
+			cmap = cmap.from_list('Custom cmap', cmaplist, cmap.N)
+			ticks = np.array([1,10,100])
+
+			kwargs = plot[m][n].check_kwargs()
+			kwargs['cmap'] = cmap
+			kwargs['norm'] = colors.LogNorm()
+			kwargs['s'] = 1
+			kwargs['lw'] = 0
+
+			# Get the data for the plots.
+			plot[m][n].get_position_arrays()
+			plot[m][n].get_magnification_array()
+			(x, y, magnification) = (plot[m][n].x_array, plot[m][n].y_array,
+						plot[m][n].magn_array)
+
+			# Create and adjust the plots appropriately.
+			ax = plt.subplot(inner[n])
+			sc = ax.scatter(x, y, c=magnification, vmin=1, vmax=100, **kwargs)
+	#		caustic = caus(lens=plot[m][n], solver='SG12')
+	#		caustic.plot_caustic(s=1, color='yellow', points=5000, lw=0)
+			get_inner_plot_parameters(plot=plot[m][n], ax=ax, k=k, l=l)
+			fig.add_subplot(ax)
+
+	# Add an axis for the color bar.
+	cbar = fig.add_axes([0.08, 0.89, 0.60, 0.04])
+	magn_color = plt.colorbar(sc, cax=cbar, cmap=kwargs['cmap'], ticks=ticks,
+							  orientation='horizontal')
+	magn_color.set_label('Magnification', fontsize=15, labelpad=-66)
+	cbar.axes.tick_params(labelsize=12)
+	get_plot_text(plot, fig)
+
+	# Save the plot as a .png file.
+	if save_fig:
+		file_name = '../../Tables/TL_magn_solver_.png'
+		save_png(file_name)
+
+	if show_fig:
+		plt.show()
+
+def get_plot_text(plot, fig):
+
+	for (i, q) in enumerate(ps_mass_ratios):
+		fig.text(0.97, 0.76 - .33/len(ps_mass_ratios) - .76*i/len(ps_mass_ratios),
+				'q1={:.0e}'.format(q), stretch='ultra-expanded', ha='center',
+				va='center', fontsize=18, rotation=90)
+
+	for (i, q) in enumerate(mp_mass_ratios):
+		fig.text(.53/len(ps_mass_ratios) + .90*i/len(ps_mass_ratios), 0.83,
+				'q2={:.0e}'.format(q), stretch='ultra-expanded', ha='center',
+				va='center', fontsize=18)
+
+	fig.text(0.74, 0.905, 's1={}, s2={}, phi={}'.format(s1, s2, phi),
+				fontsize=16)
+	plt.subplots_adjust(top=0.75, bottom=0.06, left=0.08, right=0.90)
+	plt.gcf().set_size_inches(1.8*len(solvers)*len(ps_mass_ratios)+1.5,
+							  1.8*len(origins)*len(mp_mass_ratios)+1.5)
+
+def get_inner_plot_parameters(plot, ax, k, l):
+
+	(xmin, xmax) = (min(plot.x_array), max(plot.x_array))
+	(ymin, ymax) = (min(plot.y_array), max(plot.y_array))
+	(dx, dy) = (xmax-xmin, ymax-ymin)
+	plt.xlim(xmin, xmax)
+	plt.ylim(ymin, ymax)
+	plt.xticks(np.arange(-0.3*dx, xmax, 0.6*dx))
+	plt.yticks(np.arange(-0.3*dy, ymax, 0.6*dy))
+	ax.tick_params(axis='x', labelsize=12)
+	ax.tick_params(axis='y', labelsize=12)
+
+	ax.axes.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.1e'))
+	ax.axes.xaxis.set_major_formatter(mtick.FormatStrFormatter('%.1e'))
+	labels = ax.get_yticklabels()
+	plt.setp(labels, x=0.07, rotation=60, verticalalignment='center',
+			 stretch='extra-condensed')
+	labels = ax.get_xticklabels()
+	plt.setp(labels, stretch='extra-condensed')
+
+	if (k == 0):
+		ax.axes.set_title('{}\nSolver'.format(plot.solver_title), fontsize=16)
+	if (l != 0) or (k != len(origins)-1):
+		ax.axes.get_yaxis().set_visible(False)
+		ax.axes.get_xaxis().set_visible(False)
+	if (l == len(solvers)-1):
+		title = plot.origin_title
+		xratio = 1.36
+		if title == 'Geometric Center':
+			title = 'Geometric\nCenter'
+			xratio = 1.50
+		ax.axes.text(xratio*xmax, 0.0, '{}\nFrame'.format(title), ha='center',
+				va='center', fontsize=14, rotation=90)
+
+def save_png(file_name):
+
+	for i in range(10):
+		name = file_name[:-4] + '{}'.format(i) + file_name[-4:]
+		if Path(name).is_file():
+			continue
+		plt.savefig(name, dpi=300)
+		print(name, 'has been saved')
+		return
+	print('Error: too many files of same name already exist. File not saved')
+
+
+# Here are the input parameters for making the plots.
+s1 = 1.5
+s2 = 1.0
+ps_mass_ratios = [1e-5, 1e-6]
+mp_mass_ratios = [1e-2, 1e-3]
+phi = 135
+system = 'SPM'
+
+origins = ['geo_cent', 'body2', 'body3']
+res = int(250)
+solvers =  ['numpy', 'zroots', 'SG12']
+region = 'caustic_2'
+region_lim = [-.5, .5, 0.0, 2]
+save_fig = True
+show_fig = False
+
+refine_region = True
+plot_frame = 'caustic'
+
+SFD = True
+num_images_demo()
+magnification_demo()
+
+
+ps_mass_ratios = [1e-5, 1e-6]
+mp_mass_ratios = [1e-5, 1e-6]
+system = 'SPP'
+
+num_images_demo()
+magnification_demo()
